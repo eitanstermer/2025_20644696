230d6ded5b2492026bdf28d749ec38316697f635
Eitan Stermer <eeyes6@nottingham.ac.uk>
2026-02-10 23:57:17 +0000

    Worksheet 4 Exercise 4: dynamic library with CMake

--- Worksheet4/src/adder.h
+++ Worksheet4/src/adder.h
@@ -1,3 +1,13 @@
 #pragma once
 
-int add(int a, int b);
+#ifdef _WIN32
+  #ifdef ADDER_EXPORTS
+    #define ADDER_API __declspec(dllexport)
+  #else
+    #define ADDER_API __declspec(dllimport)
+  #endif
+#else
+  #define ADDER_API
+#endif
+
+extern "C" ADDER_API int add(int a, int b);
