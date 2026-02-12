59f9417f7054b06759ba7e5aad550c29f2d980a6
Eitan Stermer <eeyes6@nottingham.ac.uk>
2026-02-12 00:05:11 +0000

    Test automatic Doxygen rebuild

--- Worksheet5/adder.h
+++ Worksheet5/adder.h
@@ -1,21 +1,10 @@
-/**
- * @brief Simple addition function used for Doxygen CI testing.
- */
-
 #ifndef ADDER_H
 #define ADDER_H
 
 /**
- * @file adder.h
- * @brief Header file for the adder library.
- */
-
-/**
  * @brief Adds two integers.
  *
- * @param a First integer
- * @param b Second integer
- * @return Sum of a and b
+ * This function is part of the Worksheet5 documentation test.
  */
 int add(int a, int b);
 
